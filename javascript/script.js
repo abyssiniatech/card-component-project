85a456d452d471313cff66b97ff0883125435125
surafelmengist <surafelmengist2015@gmail.com>
2025-07-14 13:58:22 +0300

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+const card = document.querySelector('.card');
+card.addEventListener('click',()=>{
+        document.body.style.backgroundColor ="black";
+        document.body.style.color ="white";
+})
